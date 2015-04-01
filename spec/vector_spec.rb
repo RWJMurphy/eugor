240c12ef066b7033508c4eee83937d89f25ba432
Reed Kraft-Murphy <reed@reedmurphy.net>
2015-04-01 12:55:27 +1100

    more vector tests

--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -37,7 +37,33 @@ module Eugor::Vector
       d = 10
       expect(a / d).to eq(V2.new(100, 10))
     end
+
+    it "can be modulod by a number" do
+      a = V2.new(6, 7)
+      d = 5
+      expect(a % d).to eq(V2.new(1, 2))
+    end
+
+    it "can be multiplied by a vector" do
+      a = V2.new(3, 7)
+      b = V2.new(5, 2)
+      expect(a * b).to eq(V2.new(15, 14))
+    end
+
+    it "can be divided by a vector" do
+      a = V2.new(1000, 100)
+      b = V2.new(10, 5)
+      expect(a / b).to eq(V2.new(100, 20))
+    end
+
+    it "can be modulod by a vector" do
+      a = V2.new(10, 20)
+      b = V2.new(9, 10)
+      expect(a % b).to eq(V2.new(1, 0))
+    end
   end
+
+
   RSpec.describe V3 do
     it "has three numeric properties" do
       v = V3.new(1, 5, 29)
@@ -75,5 +101,30 @@ module Eugor::Vector
       d = 10
       expect(a / d).to eq(V3.new(100, 10, 1))
     end
+
+    it "can be modulod by a number" do
+      a = V3.new(6, 7, 8)
+      d = 5
+      expect(a % d).to eq(V3.new(1, 2, 3))
+    end
+
+    it "can be multiplied by a vector" do
+      a = V3.new(3, 7, 11)
+      b = V3.new(5, 2, 3)
+      expect(a * b).to eq(V3.new(15, 14, 33))
+    end
+
+    it "can be divided by a vector" do
+      a = V3.new(1000, 100, 10)
+      b = V3.new(10, 5, 2)
+      expect(a / b).to eq(V3.new(100, 20, 5))
+    end
+
+    it "can be modulod by a vector" do
+      a = V3.new(10, 20, 30)
+      b = V3.new(9, 10, 11)
+      expect(a % b).to eq(V3.new(1, 0, 8))
+    end
+
   end
 end
